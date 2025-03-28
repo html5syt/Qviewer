31126c2047290272ebffd6585d92b951304eb333
Html5syt. <html5syt@163.com>
2025-03-28 18:28:53 +0800

    refactor(ui): 移除未使用的导入和配置类

--- main1.py
+++ main1.py
@@ -9,28 +9,7 @@ else:
     import flet_easy as fs
 app = fs.FletEasy(route_init="/")
 
-# # 导入view，用于注册路由
-# import views.home
-# import views.set
 
-# # 导入向导
-# import views.viewer
-# import views.viewer.group
-# import views.welcome
-# import views.import_wiz.import_web
-
-# if not sys.platform == "emscripten":
-#     import views.import_wiz.import_file
-
-#     app.add_routes(
-#         [
-#             fs.Pagesy(
-#                 "/import/choose",
-#                 views.import_wiz.import_file.choose,
-#                 title="Qviewer | 导入向导-选择文件",
-#             ),
-#         ]
-#     )
 
 async def home(data: fs.Datasy):
     page = data.page
@@ -83,44 +62,15 @@ async def home(data: fs.Datasy):
         appbar=appbar,
         drawer=drawer,
     )
+
 # We define the routes of the application.
 app.add_routes(
     [
-        fs.Pagesy("/", home, title="Qviewer | 首页"),
-        # fs.Pagesy("/welcome", views.welcome.welcome, title="Qviewer | 欢迎"),
-        # fs.Pagesy(
-        #     "/import/import_web",
-        #     views.import_wiz.import_web.choose,
-        #     title="Qviewer | 导入向导-手动导入JSON",
-        # ),
-        # fs.Pagesy("/set", views.set.set_page, title="Qviewer | 设置"),
-        # fs.Pagesy("/group/{group_id}", views.viewer.group.group),
+        fs.Pagesy("/",home, title="Qviewer | 首页"),
+
     ]
 )
 
-
-class ConfigApp:
-    def __init__(self, app: fs.FletEasy):
-        self.app = app
-        self.start()
-
-    def start(self):
-        @self.app.config
-        def page_config(page: ft.Page):
-            """页面通用设置"""
-            theme = ft.Theme()
-            platforms = ["android", "ios", "macos", "linux", "windows"]
-            for platform in platforms:
-                setattr(
-                    theme.page_transitions,
-                    platform,
-                    ft.PageTransitionTheme.CUPERTINO,
-                )
-            page.theme = theme
-
-
-# We load the application configuration.
-ConfigApp(app)
 
 # We run the application
 app.run()
